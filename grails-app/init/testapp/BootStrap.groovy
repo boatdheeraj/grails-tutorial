c68bee43e6a4bb7fe97186cf5090bc7eac39fee2
boatdheeraj <boatterrist@gmail.com>
2017-03-11 11:15:33 +0700

    init data

--- grails-app/init/testapp/BootStrap.groovy
+++ grails-app/init/testapp/BootStrap.groovy
@@ -3,6 +3,11 @@ package testapp
 class BootStrap {
 
     def init = { servletContext ->
+      def person = new Person(firstName: "Eric", lastName: "Helgeson")
+      person.firstName = "Bob"
+      assert person.active == false
+      person.save(flush: true)
+      Person.findByFirstName("Bob")
     }
     def destroy = {
     }
