75049169f5217ca7e5d1419fdb7b774c0656334a
boatdheeraj <boatterrist@gmail.com>
2017-03-11 12:08:01 +0700

    Edit person and bootstrap

--- grails-app/domain/testapp/Person.groovy
+++ grails-app/domain/testapp/Person.groovy
@@ -3,9 +3,14 @@ package testapp
 class Person {
     String firstName
     String lastName
+    String emailAddress
     Boolean active = false
 
     static constraints = {
+      emailAddress nullable: false, email: true, unique: true
+    }
 
+    String toString(){
+      firstName + " " + lastName
     }
 }
--- grails-app/init/testapp/BootStrap.groovy
+++ grails-app/init/testapp/BootStrap.groovy
@@ -3,7 +3,7 @@ package testapp
 class BootStrap {
 
     def init = { servletContext ->
-      def person = new Person(firstName: "Eric", lastName: "Helgeson")
+      def person = new Person(firstName: "Eric", lastName: "Helgeson", emailAddress: "1111@hotmail.com")
       person.firstName = "Bob"
       assert person.active == false
       person.save(flush: true)
